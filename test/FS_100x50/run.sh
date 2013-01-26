9d7e3bb4069c444e7b9a348c21880265bc27e632
Peter Stroia-Williams <peterDwilliams@gmail.com>
2013-01-26 23:20:00 +0000

    Added sepparate test directories for the different search methods.

---
+++ test/FS_100x50/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ../../Release/
+make all
+cd -
+../../Release/NormalRefine --FS -u ../UVStartFromFlat.exr -i ../SHDiffuse3Light_100x50.exr -s ../SphereLit.exr -o resultUV.exr --ResultShad resultShad.exr
